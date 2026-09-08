154f647a679936c3d308358a4a8bd156c62fbf3e
aa.wltr <aaron.aniambossou@epitech.eu>
2026-09-08 22:16:06 +0200

    refactor: update the name for the certification section in the sidebar

--- profilactif/apps/web/src/features/profil/components/BarreLateraleCandidat.vue
+++ profilactif/apps/web/src/features/profil/components/BarreLateraleCandidat.vue
@@ -11,7 +11,7 @@ type EntreeEspace = {
 const entrees: EntreeEspace[] = [
   { libelle: 'Tableau de bord', to: { name: 'candidate-dashboard' } },
   { libelle: 'Mon profil public', to: { name: 'candidate-public-profile' } },
-  { libelle: 'Ma certification', to: { name: 'candidate-certification' } },
+  { libelle: 'Mes certifications', to: { name: 'candidate-certification' } },
   { libelle: 'Mes interactions', to: { name: 'candidate-interactions' } },
   { libelle: 'Ma vidéo de présentation', requis: true },
   { libelle: 'Compétences & CV' },
